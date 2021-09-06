341918c1ab67d48c972062ccfc552bc3a089aee7
Philipp Peron <p.peron@tum.de>
2021-09-06 14:59:30 +0200

    Add capability to search for multiple words on a page

--- windows_boot_script.py
+++ windows_boot_script.py
@@ -9,14 +9,15 @@ from win10toast import ToastNotifier
 import time
 
 
-def check_word_in_website(website_name, url, word):
+def check_word_in_website(website_name, url, words):
     """Checks if the word is in the url's html-text."""
     site = str(urllib.request.urlopen(url).read())
-    if word in site:
-        print(f"'{word}' found on {website_name}")
-        notify(f"'{word}' found on {website_name}", " ")
-    else:
-        print(f"'{word}' not found on {website_name}")
+    for word in words:
+        if word in site:
+            print(f"'{word}' found on {website_name}")
+            notify(f"'{word}' found on {website_name}", " ")
+        else:
+            print(f"'{word}' not found on {website_name}")
 
 
 def notify(message_line_1, message_line_2):
@@ -30,13 +31,13 @@ if __name__ == '__main__':
     check_word_in_website(
         "ZHS",
         r"https://www.buchung.zhs-muenchen.de/angebote/aktueller_zeitraum_0/index.html",
-        'Wintersemester'
+        ['Wintersemester']
     )
 
-    # Cinema DUNE
+    # Cinema Kinoprogramm
     check_word_in_website(
         "Cinema",
         r"https://www.cinema-muenchen.de/filme.html",
-        '16.09.21'
+        ['16.09.21', 'James Bond', 'Top Gun', 'Maverick']
     )
 
